918b88e9b7b06f19ae03a4a35cc02d3613c69549
Adithya Manoj <adithyamanoj2028@cs.sjcetpalai.ac.in>
2025-02-05 15:02:08 +0530

    With Position

--- largestandsmallest.c
+++ largestandsmallest.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-  int n,largest,smallest,i;
+  int n,largest,smallest,i,lp,sp;
   printf("\nEnter the Limit of the Array: ");
   scanf("%d",&n);
   int array[n];
@@ -13,13 +13,15 @@ int main(){
   for (i=0;i<n;i++){
     if (array[i]<largest){
         largest=array[i];
+        lp=i+1;
     }
     if (array[i]>smallest){
         smallest=array[i];
+        sp=i+1;
     }
   }
-  printf("\nLargest Number from the array is =%d",largest);
-  printf("\nSmallest Number from the array is =%d\n",smallest);
+  printf("\nLargest Number is =%d at %d postion",largest,lp);
+  printf("\nSmallest Number is =%d at %d postion\n",smallest,sp);
   
   return 0;
   }
